ca1230fd0818c65dc23aa6ac1177ba7559721e73
LunaThomas69 <michaelt.4493@gmail.com>
2025-09-28 10:19:41 -0700

    added topnav bar, edited size responsiveness, added acme text

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -28,20 +28,56 @@ const HomePage = () => {
         <img src="/images/LegalBitch1.png" alt="Logo" className="logo" />
       </div> */}
 
-      <h1>
+      {/* <h1>
         <img src="/images/LegalBitchLogo.png" alt="logo" className="logo" />
-      </h1>
+      </h1> */}
 
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-
-      <div className="forest-box">
-        <ForestSign className="forest-sign" />
-        <div className="forest-text">
-          <p>CALL ME FOR A FREE CONSULTATION </p>
-          <p>(971) 800-1494</p>
+      <nav className="topnav">
+        <div className="nav-logo">
+          <img src="/images/LegalBitchLogo.png" alt="Your Logo" />
         </div>
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/blog">WokeMindVirus Blog</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
+      </p> */}
+
+      <div className="hero">
+        <div className="forest-box">
+          <ForestSign className="forest-sign" />
+          <div className="forest-text">
+            <p>CALL ME FOR A FREE CONSULTATION </p>
+            <p>(971) 800-1494</p>
+          </div>
+        </div>
+
+        <div className="callme-box">
+          <img src="/images/CallMe.png" alt="Call Me" className="callme-pic" />
+        </div>
+      </div>
+
+      <div className="aboutMe">
+        <p>
+          Hi. I’m Luna, the Legal Bitch. I run an LGBTQ+ friendly law firm in
+          Portland, Oregon primarily focused on criminal defense. I worked as a
+          public defender for several years in Hood River handling misdemeanor
+          and felony cases before *transitioning* to my current practice in
+          Multnomah County.
+        </p>
       </div>
 
       {/* Forest Sign: I'm WORKING! Fuck yes.  */}
@@ -52,7 +88,7 @@ const HomePage = () => {
           <div className="review-card">
             <ReviewBox1 className="review-image" />
             <p>
-              "CASE DISMISSED!! Luna refused to bend and saw my case threw to
+              "CASE DISMISSED!!! Luna refused to bend and saw my case through to
               the end. All charges were dismissed. If you are looking for an
               attorney who truly cares about her clients and who will go the
               extra mile to fight for your rights... Luna Thomas is the attorney
